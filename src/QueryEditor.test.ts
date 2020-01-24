7aa0b0600b254510626283f57198df009ea94b64
Ryan McKinley <ryantxu@gmail.com>
2020-01-23 23:57:59 -0800

    add missing files

---
+++ src/QueryEditor.test.ts
@@ -0,0 +1,16 @@
+//import { getGoogleSheetRangeInfoFromURL } from './QueryEditor';
+//
+// describe('QueryEditor', () => {
+//   it('should extract id from URL', () => {
+//     const url = 'https://docs.google.com/spreadsheets/d/1m2idieRUHdzWTu3_cpYs1lUfP_jwfgL8NBaLtqLmia8/edit#gid=790763898&range=B19:F20';
+//     const info = getGoogleSheetRangeInfoFromURL(url);
+//     expect(info.spreadsheetId).toBe('1m2idieRUHdzWTu3_cpYs1lUfP_jwfgL8NBaLtqLmia8');
+//     expect(info.range).toBe('B19:F20');
+//   });
+// });
+
+describe('placeholder test', () => {
+  it('should return true', () => {
+    expect(true).toBeTruthy();
+  });
+});
