cc43f7cfdd6e7692727f58eb24a9b828876df1ca
Ryan McKinley <ryantxu@gmail.com>
2020-01-25 09:45:35 -0800

    using canary... test passes!

--- src/QueryEditor.test.ts
+++ src/QueryEditor.test.ts
@@ -1,16 +1,10 @@
-//import { getGoogleSheetRangeInfoFromURL } from './QueryEditor';
-//
-// describe('QueryEditor', () => {
-//   it('should extract id from URL', () => {
-//     const url = 'https://docs.google.com/spreadsheets/d/1m2idieRUHdzWTu3_cpYs1lUfP_jwfgL8NBaLtqLmia8/edit#gid=790763898&range=B19:F20';
-//     const info = getGoogleSheetRangeInfoFromURL(url);
-//     expect(info.spreadsheetId).toBe('1m2idieRUHdzWTu3_cpYs1lUfP_jwfgL8NBaLtqLmia8');
-//     expect(info.range).toBe('B19:F20');
-//   });
-// });
+import { getGoogleSheetRangeInfoFromURL } from './QueryEditor';
 
-describe('placeholder test', () => {
-  it('should return true', () => {
-    expect(true).toBeTruthy();
+describe('QueryEditor', () => {
+  it('should extract id from URL', () => {
+    const url = 'https://docs.google.com/spreadsheets/d/1m2idieRUHdzWTu3_cpYs1lUfP_jwfgL8NBaLtqLmia8/edit#gid=790763898&range=B19:F20';
+    const info = getGoogleSheetRangeInfoFromURL(url);
+    expect(info.spreadsheetId).toBe('1m2idieRUHdzWTu3_cpYs1lUfP_jwfgL8NBaLtqLmia8');
+    expect(info.range).toBe('B19:F20');
   });
 });
