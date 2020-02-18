0a45237da756e9117d109db9ccefb5c5fce0ba2b
Erik Sundell <erik.sundell87@gmail.com>
2020-02-18 13:04:54 +0100

    Cleanup. Use segment for spreadsheetid

--- src/QueryEditor.tsx
+++ src/QueryEditor.tsx
@@ -66,19 +66,6 @@ export class QueryEditor extends PureComponent<Props, State> {
     }
   };
 
-  onSpreadsheetIdChange = (event: ChangeEvent<HTMLInputElement>) => {
-    console.log('CHANGE', event.target.value);
-    let v = event.target.value;
-    const idx = v.indexOf(PASTE_SEPERATOR);
-    if (idx > 0) {
-      v = v.substring(0, idx);
-    }
-    this.props.onChange({
-      ...this.props.query,
-      spreadsheetId: v,
-    });
-  };
-
   onRangeChange = (event: ChangeEvent<HTMLInputElement>) => {
     this.props.onChange({
       ...this.props.query,
@@ -109,31 +96,12 @@ export class QueryEditor extends PureComponent<Props, State> {
             value={query.spreadsheetId || ''}
             allowCustomValue={true}
             onChange={({ value }) => {
-              console.log({ value });
-              onChange({ ...query, spreadsheetId: value! });
+              /(.*)\/spreadsheets\/d\/(.*)/.test(value!)
+                ? onChange({ ...query, ...getGoogleSheetRangeInfoFromURL(value!) })
+                : onChange({ ...query, spreadsheetId: value! });
               onRunQuery();
             }}
           ></SegmentAsync>
-          <FormLabel
-            width={10}
-            className="query-keyword"
-            tooltip={
-              <p>
-                The <code>spreadsheetId</code> is used to identify which spreadsheet is to be accessed or altered. This ID is the value between the
-                "/d/" and the "/edit" in the URL of your spreadsheet.
-              </p>
-            }
-          >
-            Spreadsheet ID
-          </FormLabel>
-          <input
-            className="gf-form-input width-28"
-            placeholder="Enter ID from URL"
-            value={query.spreadsheetId || ''}
-            onPaste={this.onSpreadsheetIdPasted}
-            onChange={this.onSpreadsheetIdChange}
-            onBlur={onRunQuery}
-          ></input>
           <LinkButton disabled={!query.spreadsheetId} variant="secondary" icon="fa fa-link" href={toGoogleURL(query)} target="_blank"></LinkButton>
           <div className="gf-form gf-form--grow">
             <div className="gf-form-label gf-form-label--grow" />
