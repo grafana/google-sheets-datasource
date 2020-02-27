2efeb51c677ee5c4b6e0a3da256b9cc001d652a2
Arve Knudsen <arve.knudsen@gmail.com>
2020-02-27 15:29:29 +0100

    restart-plugin.sh: Fix killing
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- scripts/restart-plugin.sh
+++ scripts/restart-plugin.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -eo pipefail
 
 make build
-docker exec google-sheets-datasource_grafana_1 pkill -f "/var/lib/grafana/plugins/google_sheets_datasource/dist/sheets_datasource_linux_amd64"
+docker exec google-sheets-datasource_grafana_1 pkill -f "/var/lib/grafana/plugins/google-sheets-datasource/dist/sheets-datasource_linux_amd64"
