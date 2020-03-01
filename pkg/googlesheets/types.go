a51ad5bd9f7d11f693067414868f15e967b6cb6b
Arve Knudsen <arve.knudsen@gmail.com>
2020-03-01 13:13:38 +0100

    Fix linting issues
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- pkg/googlesheets/types.go
+++ pkg/googlesheets/types.go
@@ -4,19 +4,22 @@ import (
 	"google.golang.org/api/sheets/v4"
 )
 
+// Spreadsheet represents a Google spreadsheet.
 type Spreadsheet struct {
 	ID   string `json:"value"`
 	Name string `json:"label"`
 }
 
+// QueryModel represents a spreadsheet query.
 type QueryModel struct {
 	Spreadsheet          Spreadsheet `json:"Spreadsheet"`
 	Range                string      `json:"range"`
 	CacheDurationSeconds int         `json:"cacheDurationSeconds"`
 }
 
+// GoogleSheetConfig contains Google Sheets API authentication properties.
 type GoogleSheetConfig struct {
-	ApiKey   string `json:"apiKey"`
+	APIKey   string `json:"apiKey"`
 	AuthType string `json:"authType"`
 	JWT      string `json:"jwt"`
 }
