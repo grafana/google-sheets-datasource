309ef87d033f7749edc48966272817ec05c20309
Arve Knudsen <arve.knudsen@gmail.com>
2020-03-02 17:30:48 +0100

    Don't embed plugin.NetRPCUnsupportedPlugin (#20)
    * Don't embed plugin.NetRPCUnsupportedPlugin
    * Update yarn.lock
    * Upgrade dependencies

--- pkg/datasource.go
+++ pkg/datasource.go
@@ -14,9 +14,8 @@ import (
 	"github.com/patrickmn/go-cache"
 
 	hclog "github.com/hashicorp/go-hclog"
-	plugin "github.com/hashicorp/go-plugin"
 
-	"golang.org/x/net/context"
+	"context"
 )
 
 const (
@@ -37,18 +36,16 @@ func main() {
 			Logger: pluginLogger,
 		},
 	}
-	err := backend.Serve(backend.ServeOpts{
+	if err := backend.Serve(backend.ServeOpts{
 		DataQueryHandler:    ds,
 		CallResourceHandler: ds,
-	})
-	if err != nil {
+	}); err != nil {
 		pluginLogger.Error(err.Error())
 		os.Exit(1)
 	}
 }
 
 type googleSheetsDataSource struct {
-	plugin.NetRPCUnsupportedPlugin
 	logger      hclog.Logger
 	googlesheet *googlesheets.GoogleSheets
 }
