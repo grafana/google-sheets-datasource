2b37cbe6f8e25a0253ac6ecb8a1607fcafb61360
Ryan McKinley <ryantxu@gmail.com>
2020-03-16 13:00:52 -0700

    add missing file

---
+++ pkg/types.go
@@ -0,0 +1,18 @@
+package main
+
+// QueryModel represents a spreadsheet query.
+// This needs to match the request sent from:
+// https://github.com/grafana/google-sheets-datasource/blob/master/src/types.ts#L46
+type QueryModel struct {
+	Spreadsheet          string `json:"spreadsheet"`
+	Range                string `json:"range"`
+	CacheDurationSeconds int    `json:"cacheDurationSeconds"`
+	UseTimeFilter        bool   `json:"useTimeFilter"`
+}
+
+// GoogleSheetConfig contains Google Sheets API authentication properties.
+type GoogleSheetConfig struct {
+	AuthType string `json:"authType"` // jwt | key
+	APIKey   string `json:"apiKey"`   // Saved in secure JSON fields
+	JWT      string `json:"jwt"`      // Saved in secure JSON fields
+}
