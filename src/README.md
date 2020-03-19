5d7b8b3a8bd1f7ed56cc62087aadd5f391827673
Erik Sundell <erik.sundell87@gmail.com>
2020-03-19 15:26:50 +0100

    wip - add docs and images

---
+++ src/README.md
@@ -0,0 +1,15 @@
+# Google Sheets Datasource
+
+Visualize your Google Spreadsheets in Grafana!
+
+![Visualize temperature date in Grafana Google Spreadsheets data source](./img/dashboard.png)
+
+![Average temperatures in Google Sheets](./img/spreadsheet.png)
+
+## Configuration
+
+Please refer to the [configuration](./docs/configuration.md) document.
+
+## Using the editor
+
+Please refer to the [Using the editor](./docs/using-the-editor.md) document.
--- src/components/ConfigEditor.tsx
+++ src/components/ConfigEditor.tsx
@@ -88,7 +88,14 @@ export class ConfigEditor extends PureComponent<Props> {
                 </li>
                 <li>On the Create service account page, enter the Service account details.</li>
                 <li>
-                  Click <strong>Create</strong>. A JSON key file is created and downloaded to your computer.
+                  On the <code>Create service account</code> page, fill in the <code>Service account details</code> and then click <code>Create</code>
+                </li>
+                <li>
+                  On the <code>Service account permissions</code> page, don't add a role to the service account. Just click <code>Continue</code>
+                </li>
+                <li>
+                  In the next step, click <code>Create Key</code>. Choose key type <code>JSON</code> and click <code>Create</code>. A JSON key file
+                  will be created and downloaded to your computer
                 </li>
                 <li>
                   Open the <a href="https://console.cloud.google.com/apis/library/sheets.googleapis.com?q=sheet">Google Sheets</a> in API Library and
@@ -98,7 +105,10 @@ export class ConfigEditor extends PureComponent<Props> {
                   Open the <a href="https://console.cloud.google.com/apis/library/drive.googleapis.com?q=drive">Google Drive</a> in API Library and
                   enable access for your account. Access to the Google Drive API is used to list all spreadsheets that you have access to.
                 </li>
-                <li>Drag the file to the dotted zone below. The file contents are encrypted and saved in the Grafana database.</li>
+                <li>
+                  Drag the file to the dotted zone above. Then click <code>Save & Test</code>. The file contents will be encrypted and saved in the
+                  Grafana database.
+                </li>
               </ol>
             </>
           ) : (
