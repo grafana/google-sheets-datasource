1e50d97f3bfd40870997039b17c0c8c901f8fbf8
Steven Vachon <contact@svachon.com>
2020-03-31 15:14:00 -0400

    E2E tests (#52)
    * Rename "test_integration" CI job
    
    ... because integration tests are different from E2E tests
    
    * Reduce noise in CI log
    
    * Use latest Grafana version in CI
    
    * Checkout provisioning files in CI
    
    * Use CircleCI config version 2.1
    
    … for some more modern convenience
    
    * Always copy artifacts, even when e2e tests fail
    
    … now that @grafana/e2e correctly exits with a non-0 code upon failure.
    
    * Added E2E tests
    
    * Use canary packages for now

--- cypress/integration/something.ts
+++ cypress/integration/something.ts
@@ -1,10 +1,58 @@
+/// <reference path="../../node_modules/@grafana/e2e/cypress/support/index.d.ts" />
 import { e2e } from '@grafana/e2e';
+
+const addGoogleSheetsDataSource = (apiKey: string) => {
+  const fillApiKey = () => getByPlaceholder('Enter API Key').scrollIntoView().type(apiKey);
+
+  // This get auto-removed within `afterEach` of @grafana/e2e
+  e2e.flows.addDataSource({
+    checkHealth: true,
+    expectedAlertMessage: 'Success',
+    form: () => fillApiKey(),
+    name: 'Google Sheets',
+  });
+};
+
+const addGoogleSheetsPanel = (spreadsheetId: string) => {
+  const fillSpreadsheetID = () => {
+    e2e()
+      .contains('.gf-form-label', 'Enter SpreadsheetID')
+      .parent('.gf-form') // the <Label/>
+      .click();
+    e2e()
+      .contains('.gf-form-input', 'Choose')
+      .find('.gf-form-select-box__input input')
+      .scrollIntoView()
+      .type(spreadsheetId);
+  };
+
+  // @todo remove `@ts-ignore` when possible
+  // @ts-ignore
+  e2e.getScenarioContext().then(({ lastAddedDataSource }) => {
+    // This get auto-removed within `afterEach` of @grafana/e2e
+    e2e.flows.addPanel({
+      dataSourceName: lastAddedDataSource,
+      queriesForm: () => fillSpreadsheetID(),
+    });
+  });
+};
+
+export const getByPlaceholder = (placeholder: string) => e2e().get(`[placeholder="${placeholder}"]`);
 
 e2e.scenario({
   describeName: 'Smoke tests',
-  itName: 'Login scenario, create test data source, dashboard, panel, and export scenario',
-  addScenarioDataSource: true,
-  addScenarioDashBoard: true,
-  skipScenario: false,
-  scenario: () => {},
+  itName: 'Login, create data source, dashboard and panel',
+  scenario: () => {
+    // Paths are relative to <project-root>/provisioning
+    const provisionPaths = [
+      'datasources/google-sheets-datasource-API-key.yml',
+      'datasources/google-sheets-datasource-jwt.yml',
+    ];
+
+    e2e().readProvisions(provisionPaths).then(([apiKeyProvision, jwtProvision]) => {
+      addGoogleSheetsDataSource(apiKeyProvision.datasources[0].secureJsonData.apiKey);
+      e2e.flows.addDashboard();
+      addGoogleSheetsPanel('1Kn_9WKsuT-H0aJL3fvqukt27HlizMLd-KQfkNgeWj4U');
+    });
+  },
 });
