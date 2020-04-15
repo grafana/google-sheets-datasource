9adc5429cec6cccb3dae31c1b6c5ae151f77866b
Arve Knudsen <arve.knudsen@gmail.com>
2020-04-15 17:24:04 +0200

    Magefile: Fix default target
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- Magefile.go
+++ Magefile.go
@@ -3,16 +3,9 @@
 package main
 
 import (
-	"fmt"
-
 	// mage:import
-	_ "github.com/grafana/grafana-plugin-sdk-go/build"
+	build "github.com/grafana/grafana-plugin-sdk-go/build"
 )
 
-// CheckCheck builds production back-end components.
-func CheckCheck() {
-	fmt.Println("hello! TODO.... (the other commands are imported)")
-}
-
 // Default configures the default target.
-var Default = CheckCheck
+var Default = build.BuildAll
