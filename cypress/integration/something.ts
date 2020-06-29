19b2af4b94961637a01800d22461b42a65c84460
Steven Vachon <contact@svachon.com>
2020-06-29 12:22:53 -0400

    E2E improvements (#101)
    * Updates for changes to @grafana/e2e
    
    * Minor changes
    
    * Added JWT test
    
    * Disabled JWT test for now
    
    ... due to https://github.com/cypress-io/cypress/issues/7728
    
    * Updated lockfile

--- cypress/integration/something.ts
+++ cypress/integration/something.ts
@@ -1,52 +1,55 @@
 /// <reference path="../../node_modules/@grafana/e2e/cypress/support/index.d.ts" />
 import { e2e } from '@grafana/e2e';
 
-const addGoogleSheetsDataSource = (apiKey: string) => {
-  const fillApiKey = () => getByPlaceholder('Enter API Key').scrollIntoView().type(apiKey);
+const addGoogleSheetsDataSource = (config: { apiKey: string } | { jwtPath: string }) => {
+  const fillApiKey = (apiKey: string) => getByPlaceholder('Enter API Key').scrollIntoView().type(apiKey);
+
+  const fillJwt = (jwtPath: string) => {
+    e2e().contains('.gf-form-group', 'Auth').within(() => {
+      e2e.flows.selectOption(e2e.components.Select.option(), 'Google JWT File');
+      e2e().get('input[type=file]').attachFile(jwtPath);
+    });
+  };
 
   // This gets auto-removed within `afterEach` of @grafana/e2e
   e2e.flows.addDataSource({
     checkHealth: true,
     expectedAlertMessage: 'Success',
-    form: () => fillApiKey(),
-    name: 'Google Sheets',
+    form: () => {
+      if ('apiKey' in config) {
+        fillApiKey(config.apiKey);
+      } else if ('jwtPath' in config) {
+        fillJwt(config.jwtPath);
+      }
+    },
+    type: 'Google Sheets',
   });
 };
 
 const addGoogleSheetsPanel = (spreadsheetId: string) => {
   const fillSpreadsheetID = () => {
-    const getContainer = e2e.components.QueryTab.content;
+    e2e.components.QueryTab.content().within(() => {
+      e2e()
+        .contains('.gf-form-label', 'Enter SpreadsheetID')
+        .parent('.gf-form') // the <Label/>
+        .click({ force: true }); // https://github.com/cypress-io/cypress/issues/7306
 
-    e2e().server();
-    e2e().route('POST', '/api/ds/query').as('chartData');
-
-    getContainer()
-      .contains('.gf-form-label', 'Enter SpreadsheetID')
-      .parent('.gf-form') // the <Label/>
-      .click({ force: true }); // https://github.com/cypress-io/cypress/issues/7306
-
-    getContainer()
-      .contains('.gf-form-input', 'Choose')
-      .find('.gf-form-select-box__input input')
-      .scrollIntoView()
-      .type(`${spreadsheetId}{enter}`);
-
-    e2e().wait('@chartData');
+      e2e()
+        .contains('.gf-form-input', 'Choose')
+        .find('.gf-form-select-box__input input')
+        .scrollIntoView()
+        .type(`${spreadsheetId}{enter}`);
+    });
   };
 
-  // @todo remove `@ts-ignore` when possible
-  // @ts-ignore
-  e2e.getScenarioContext().then(({ lastAddedDataSource }) => {
-    // This gets auto-removed within `afterEach` of @grafana/e2e
-    e2e.flows.addPanel({
-      dataSourceName: lastAddedDataSource,
-      queriesForm: () => fillSpreadsheetID(),
-      visualizationName: 'Table',
-    }).then((panelTitle: string) => {
-      e2e.components.PanelEditor.OptionsPane.close().click();
-      e2e.components.Panels.Panel.containerByTitle(panelTitle).find('.panel-content').screenshot('chart');
-      e2e().compareScreenshots('chart');
-    });
+  // This gets auto-removed within `afterEach` of @grafana/e2e
+  e2e.flows.addPanel({
+    queriesForm: () => fillSpreadsheetID(),
+  }).then(({ config }: any) => {
+    e2e.components.Panels.Panel.containerByTitle(config.panelTitle)
+      .find('.panel-content')
+      .screenshot('chart');
+    e2e().compareScreenshots('chart');
   });
 };
 
@@ -56,16 +59,25 @@ e2e.scenario({
   describeName: 'Smoke tests',
   itName: 'Login, create data source, dashboard and panel',
   scenario: () => {
-    // Paths are relative to <project-root>/provisioning
-    const provisionPaths = [
+    e2e().readProvisions([
+      // Paths are relative to <project-root>/provisioning
       'datasources/google-sheets-datasource-API-key.yml',
       'datasources/google-sheets-datasource-jwt.yml',
-    ];
+    ]).then(([apiKeyProvision, jwtProvision]) => {
+      const { apiKey } = apiKeyProvision.datasources[0].secureJsonData;
+      //const { jwt } = jwtProvision.datasources[0].secureJsonData;
+      const sheetId = '1Kn_9WKsuT-H0aJL3fvqukt27HlizMLd-KQfkNgeWj4U';
 
-    e2e().readProvisions(provisionPaths).then(([apiKeyProvision, jwtProvision]) => {
-      addGoogleSheetsDataSource(apiKeyProvision.datasources[0].secureJsonData.apiKey);
+      //const jwtPath = 'jwt.json';
+      //e2e().writeFile(`${Cypress.config('fixturesFolder')}/${jwtPath}`, jwt);
+
+      addGoogleSheetsDataSource({ apiKey });
       e2e.flows.addDashboard();
-      addGoogleSheetsPanel('1Kn_9WKsuT-H0aJL3fvqukt27HlizMLd-KQfkNgeWj4U');
+      addGoogleSheetsPanel(sheetId);
+
+      //addGoogleSheetsDataSource({ jwtPath });
+      //e2e.flows.addDashboard();
+      //addGoogleSheetsPanel(sheetId);
     });
   },
 });
