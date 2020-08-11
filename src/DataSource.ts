59e4249234b4ea20ccd2959ff219e5e6e1817c86
Mat Baker <matthew.baker@engauged.io>
2020-08-11 13:34:37 +1000

    added basic templating support for v0.9.X as per v1.0 release.

--- src/DataSource.ts
+++ src/DataSource.ts
@@ -1,11 +1,21 @@
 import { DataSourceInstanceSettings, SelectableValue } from '@grafana/data';
-import { DataSourceWithBackend } from '@grafana/runtime';
+import { DataSourceWithBackend, getDataSourceSrv } from '@grafana/runtime';
 
 import { SheetsQuery, SheetsSourceOptions } from './types';
 
 export class DataSource extends DataSourceWithBackend<SheetsQuery, SheetsSourceOptions> {
   constructor(instanceSettings: DataSourceInstanceSettings<SheetsSourceOptions>) {
     super(instanceSettings);
+  }
+
+  // Support template variables for spreadsheet and range
+  applyTemplateVariables(query: SheetsQuery) {
+    let templateSrv = (getDataSourceSrv() as any).templateSrv;
+    return {
+      ...query,
+      spreadsheet: templateSrv.replace(query.spreadsheet),
+      range: templateSrv.replace(query.range),
+    };
   }
 
   async getSpreadSheets(): Promise<Array<SelectableValue<string>>> {
