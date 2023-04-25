d0e9217ef67d7ba199f159d89ea9d723c02f7efb
Ida Štambuk <ida.stambuk@grafana.com>
2023-04-25 10:27:17 +0200

    Add test for selecting spreadsheet

--- src/components/ConfigEditor.test.tsx
+++ src/components/ConfigEditor.test.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import { ConfigEditor } from './ConfigEditor';
-import { DataSourcePluginOptionsEditorProps, DataSourceSettings } from '@grafana/data';
+import {
+  DataSourcePluginOptionsEditorProps,
+  DataSourceSettings,
+  onUpdateDatasourceJsonDataOptionSelect,
+} from '@grafana/data';
 import { SheetsSourceOptions, GoogleSheetsSecureJsonData, GoogleAuthType } from '../types';
+import userEvent from '@testing-library/user-event';
+import exp from 'constants';
 
 const dataSourceSettings: DataSourceSettings<SheetsSourceOptions, GoogleSheetsSecureJsonData> = {
   jsonData: {
@@ -34,18 +40,28 @@ const dataSourceSettings: DataSourceSettings<SheetsSourceOptions, GoogleSheetsSe
 
 jest.mock('@grafana/runtime', () => ({
   getDataSourceSrv: () => ({
-    get: Promise.resolve({
-      getSpreadSheets: jest.fn().mockImplementation(() =>
-        Promise.resolve([
-          { label: 'label1', value: 'value1' },
-          { label: 'label2', value: 'value2' },
-        ])
-      ),
-    }),
+    get: (_: string) =>
+      Promise.resolve({
+        getSpreadSheets: () =>
+          Promise.resolve([
+            { label: 'label1', value: 'value1' },
+            { label: 'label2', value: 'value2' },
+          ]),
+      }),
   }),
+}));
+const mockedSelect = jest.fn();
+jest.mock('@grafana/data', () => ({
+  ...jest.requireActual('@grafana/data'),
+  // onUpdateDatasourceJsonDataOptionSelect will be called anyway on mount, and twice, one for spreadsheetId and one for authType. 
+  // The one we are actually testing for is the returned function (in this case mockedSelect) and checking that it is called with the value of the selected option
+  onUpdateDatasourceJsonDataOptionSelect: jest.fn(() => mockedSelect),
 }));
 
 describe('ConfigEditor', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
   it('should render default spreadsheet ID field', async () => {
     const onChange = jest.fn();
     const props = {
@@ -56,7 +72,7 @@ describe('ConfigEditor', () => {
     expect(screen.getByText('Default spreadsheet ID')).toBeInTheDocument();
   });
 
-  it('should display available spreadsheets in selector', async () => {
+  it('should update default spreadsheet after selecting it', async () => {
     const onChange = jest.fn();
     const props = {
       options: dataSourceSettings,
@@ -65,9 +81,11 @@ describe('ConfigEditor', () => {
 
     render(<ConfigEditor {...props} onOptionsChange={onChange} />);
 
-    waitFor(() => {
-      const selectEl = screen.getByText('Default spreadsheet ID');
-      expect(selectEl).not.toBeInTheDocument();
-    });
+    const selectEl = screen.getByText('Select Spreadsheet ID');
+    expect(selectEl).toBeInTheDocument();
+    userEvent.click(selectEl);
+    const spreadsheetOption = await screen.findByText('label1');
+    userEvent.click(spreadsheetOption);
+    await waitFor(() => expect(mockedSelect).toHaveBeenCalledWith({ label: 'label1', value: 'value1' }));
   });
 });
