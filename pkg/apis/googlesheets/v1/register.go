f39b23b9e56f2b619abc4ee597c8d664744affd3
Charandas Batra <charandas.batra@grafana.com>
2023-08-15 22:21:40 -0700

    fix: this is what was missing in scheme for base resource

--- pkg/apis/googlesheets/v1/register.go
+++ pkg/apis/googlesheets/v1/register.go
@@ -37,6 +37,13 @@ var (
 	AddToScheme = localSchemeBuilder.AddToScheme
 )
 
+func init() {
+	// We only register manually written functions here. The registration of the
+	// generated functions takes place in the generated files. The separation
+	// makes the code compile even when the generated files are missing.
+	localSchemeBuilder.Register(addKnownTypes)
+}
+
 // Kind takes an unqualified kind and returns back a Group qualified GroupKind
 func Kind(kind string) schema.GroupKind {
 	return SchemeGroupVersion.WithKind(kind).GroupKind()
