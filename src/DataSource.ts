b7f0f155f774a24f8b3fb466edf12120cd8dedf0
Gareth Dawson <gwdawson.work@gmail.com>
2023-09-06 11:21:48 +0100

    report usage

--- src/DataSource.ts
+++ src/DataSource.ts
@@ -1,13 +1,32 @@
-import { DataSourceInstanceSettings, ScopedVars, SelectableValue } from '@grafana/data';
+import {
+  DataQueryRequest,
+  DataQueryResponse,
+  DataSourceInstanceSettings,
+  ScopedVars,
+  SelectableValue,
+} from '@grafana/data';
 import { DataSourceOptions } from '@grafana/google-sdk';
-import { DataSourceWithBackend, getTemplateSrv } from '@grafana/runtime';
+import { DataSourceWithBackend, getTemplateSrv, reportInteraction } from '@grafana/runtime';
 import { SheetsQuery } from './types';
+import { Observable } from 'rxjs';
 
 export class DataSource extends DataSourceWithBackend<SheetsQuery, DataSourceOptions> {
   authType: string;
   constructor(instanceSettings: DataSourceInstanceSettings<DataSourceOptions>) {
     super(instanceSettings);
     this.authType = instanceSettings.jsonData.authenticationType;
+  }
+
+  query(request: DataQueryRequest<SheetsQuery>): Observable<DataQueryResponse> {
+    request.targets.forEach((target) => {
+      reportInteraction('grafana_google_sheets_query_executed', {
+        app: request.app,
+        useTimeFilter: target.useTimeFilter ?? false,
+        cacheDurationSeconds: target.cacheDurationSeconds ?? 0,
+      });
+    });
+
+    return super.query(request);
   }
 
   // Enables default annotation support for 7.2+
