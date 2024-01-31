a8576d12e90016e90b276bb3c566d6f6c6af6d66
Erik Sundell <erik.sundell87@gmail.com>
2024-01-31 10:14:59 +0100

    w

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,17 +1,23 @@
 import { test, expect } from '@grafana/plugin-e2e';
 
-test('should return data and not display panel error when a valid query is provided', async ({
-  explorePage,
+const GOOGLE_SHEETS_SPREADSHEETS = {
+  spreadsheets: {
+    sheet1: 'Datasource test spreadsheet',
+    sheet2: 'Google Sheets Datasource - Average Temperature',
+  },
+};
+
+test('should list spreadsheets when clicking on spreadsheet segment', async ({
+  panelEditPage,
   page,
   readProvision,
 }) => {
-  const provision = await readProvision({ filePath: 'datasources/google-sheets-datasource-jwt.yaml' });
-  await explorePage.datasource.set(provision.datasources?.[0]!.name!);
-  await explorePage.timeRange.set({ from: '2019-01-11', to: '2019-12-15' });
-  await explorePage.getQueryEditorRow('A').getByText('Enter SpreadsheetID').click();
-  await page.keyboard.insertText('1TZlZX67Y0s4CvRro_3pCYqRCKuXer81oFp_xcsjPpe8');
-  const responsePromise = page.waitForResponse((resp) => resp.url().includes('/api/ds/query'));
-  await page.keyboard.press('Tab');
-  await responsePromise;
-  await expect(explorePage.runQuery()).toBeOK();
+  const sheetsDataSource = await readProvision({
+    filePath: 'datasources/google-sheets-datasource-jwt.yaml',
+  }).then((provision) => provision.datasources?.[0]!);
+  await panelEditPage.datasource.set(sheetsDataSource.name!);
+  await panelEditPage.mockResourceResponse('spreadsheets', GOOGLE_SHEETS_SPREADSHEETS);
+  await panelEditPage.getQueryEditorRow('A').getByText('Enter SpreadsheetID').click();
+  await expect(page.getByText(GOOGLE_SHEETS_SPREADSHEETS.spreadsheets.sheet1, { exact: true })).toHaveCount(1);
+  await expect(page.getByText(GOOGLE_SHEETS_SPREADSHEETS.spreadsheets.sheet2, { exact: true })).toHaveCount(1);
 });
