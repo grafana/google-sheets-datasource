4792400051dc5c04d3ebb0ed89a860c35a85c44c
Sriramajeyam Sugumaran <153843+yesoreyeram@users.noreply.github.com>
2024-04-03 07:26:07 +0100

    dependencies update

---
+++ .config/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if grep -i -q alpine /etc/issue; then
+    exec /usr/bin/supervisord -c /etc/supervisord.conf
+elif grep -i -q ubuntu /etc/issue; then
+    exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
+else
+    echo 'ERROR: Unsupported base image'
+    exit 1
+fi
