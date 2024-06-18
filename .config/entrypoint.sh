6a769d8099ef5e0fcf49e3bcbb57ca90997df607
Zoltán Bedi <zoltan.bedi@gmail.com>
2024-06-19 00:20:08 +0200

    Update create plugin to latest

--- .config/entrypoint.sh
+++ .config/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if [ "${DEV}" = "false" ]; then
+    echo "Starting test mode"
+    exec /run.sh
+fi
+
+echo "Starting development mode"
+
 if grep -i -q alpine /etc/issue; then
     exec /usr/bin/supervisord -c /etc/supervisord.conf
 elif grep -i -q ubuntu /etc/issue; then
@@ -8,3 +15,4 @@ else
     echo 'ERROR: Unsupported base image'
     exit 1
 fi
+
