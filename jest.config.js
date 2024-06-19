44d0b7fb135c9dd4aab6c0e27931c5344137310e
Sriram <153843+yesoreyeram@users.noreply.github.com>
2024-06-19 07:09:15 +0100

    dependencies update (#236)
    * dependencies update
    
    * Update create plugin to latest
    
    * Add missing EOL
    
    * Remove unnecessary config
    
    * package.json changes
    
    * Fix test warning
    
    ---------
    
    Co-authored-by: Zoltán Bedi <zoltan.bedi@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,6 @@
 process.env.TZ = 'UTC';
-const { grafanaESModules, nodeModulesToTransform } = require('./.config/jest/utils');
 
 module.exports = {
   // Jest configuration provided by @grafana/create-plugin
   ...require('./.config/jest.config'),
-  // Inform jest to only transform specific node_module packages.
-  transformIgnorePatterns: [
-    nodeModulesToTransform([...grafanaESModules, 'rxjs', '@grafana/ui', '@grafana/experimental']),
-  ],
 };
