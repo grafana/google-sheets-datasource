2f2fba21dfe13b68a079c3d110f76a3a8d172b0d
yesoreyeram <153843+yesoreyeram@users.noreply.github.com>
2025-06-20 06:08:35 +0100

    update config page description with text links

--- src/components/ConfigEditor.tsx
+++ src/components/ConfigEditor.tsx
@@ -1,6 +1,6 @@
 import { DataSourcePluginOptionsEditorProps, onUpdateDatasourceSecureJsonDataOption } from '@grafana/data';
 import { AuthConfig, DataSourceOptions } from '@grafana/google-sdk';
-import { Field, SecretInput, Divider } from '@grafana/ui';
+import { Field, SecretInput, Divider, TextLink } from '@grafana/ui';
 import React from 'react';
 import { GoogleSheetsAuth, GoogleSheetsSecureJSONData, googleSheetsAuthTypes } from '../types';
 import { getBackwardCompatibleOptions } from '../utils';
@@ -37,16 +37,40 @@ export function ConfigEditor(props: Props) {
 
       <Divider />
       <div className="grafana-info-box">
-        <h5>Choosing an authentication type</h5>
+        <h5>Choosing an Authentication Type</h5>
         <p>
-          <strong>Google JWT File</strong>: provides access to private spreadsheets and works in all environments where Grafana is running. 
-          <br/>
-          <strong>API Key</strong>: simpler configuration, but requires spreadsheets to be public.
-          <br/>
-          <strong>GCE Default Service Account</strong>: automatically retrieves default credentials. Requires Grafana to be running on a Google Compute Engine virtual machine.
-          <br/><br/>
-          <strong>Select an Authentication type below and expand <strong>Configure Google Sheets Authentication</strong> for 
-          detailed guidance on configuration</strong>.
+          <TextLink
+            href="https://grafana.com/docs/plugins/grafana-googlesheets-datasource/latest/setup/configure/#authenticate-with-a-service-account-jwt"
+            external
+          >
+            Google JWT File
+          </TextLink>
+          : Provides access to private spreadsheets and works in all environments where Grafana is running. <br />
+          <br />
+          <TextLink
+            href="https://grafana.com/docs/plugins/grafana-googlesheets-datasource/latest/setup/configure/#authenticate-with-an-api-key"
+            external
+          >
+            API Key
+          </TextLink>
+          : Simpler configuration, but requires spreadsheets to be public.
+          <br />
+          <br />
+          <TextLink
+            href="https://grafana.com/docs/plugins/grafana-googlesheets-datasource/latest/setup/configure/#authenticate-with-the-default-gce-service-account"
+            external
+          >
+            GCE Default Service Account
+          </TextLink>
+          : Automatically retrieves default credentials. Requires Grafana to be running on a Google Compute Engine
+          virtual machine.
+          <br />
+          <br />
+          <strong>
+            Select an Authentication type below and expand <strong>Configure Google Sheets Authentication</strong> for
+            detailed guidance on configuration
+          </strong>
+          .
         </p>
       </div>
       <ConfigurationHelp authenticationType={options.jsonData.authenticationType} />
