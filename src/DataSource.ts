8118de5d76fb176700407a71b8814394e2583dce
Shirley Leu <4163034+fridgepoet@users.noreply.github.com>
2025-12-03 13:22:55 +0100

    wip

--- src/DataSource.ts
+++ src/DataSource.ts
@@ -64,7 +64,10 @@ export class DataSource extends DataSourceWithBackend<SheetsQuery, DataSourceOpt
   async getSpreadSheets(): Promise<Array<SelectableValue<string>>> {
     return this.getResource('spreadsheets').then(({ spreadsheets }) =>
       spreadsheets
-        ? Object.entries(spreadsheets).map(([value, label]) => ({ label, value }) as SelectableValue<string>)
+        ? Object.entries(spreadsheets).map(([value, label]) => ({
+            label: `${label} (${value})`,
+            value,
+          }) as SelectableValue<string>)
         : []
     );
   }
