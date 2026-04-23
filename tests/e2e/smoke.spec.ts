05afe6047038f5889de980900a9cc715360fef9d
Zoltán Bedi <zoltan.bedi@gmail.com>
2026-04-23 10:16:05 +0200

    Chore: E2E fix and various changes

--- tests/e2e/smoke.spec.ts
+++ tests/e2e/smoke.spec.ts
@@ -4,5 +4,5 @@ test('Smoke test: plugin loads', async ({ createDataSourceConfigPage, page }) =>
   await createDataSourceConfigPage({ type: 'grafana-googlesheets-datasource' });
 
   await expect(await page.getByText('Type: Google Sheets', { exact: true })).toBeVisible();
-  await expect(await page.locator('legend', { hasText: 'Authentication' })).toBeVisible();
+  await expect(page.getByRole('group', { name: 'Authentication', exact: true })).toBeVisible();
 });
