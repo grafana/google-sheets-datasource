761a551bfa14d5e390d1cfcd33bb674c505c318f
Zoltán Bedi <zoltan.bedi@gmail.com>
2026-05-28 13:41:57 +0200

    Fix E2E tests (#579)

--- tests/e2e/smoke.spec.ts
+++ tests/e2e/smoke.spec.ts
@@ -3,6 +3,5 @@ import { test, expect } from '@grafana/plugin-e2e';
 test('Smoke test: plugin loads', async ({ createDataSourceConfigPage, page }) => {
   await createDataSourceConfigPage({ type: 'grafana-googlesheets-datasource' });
 
-  await expect(await page.getByText('Type: Google Sheets', { exact: true })).toBeVisible();
   await expect(page.getByRole('group', { name: 'Authentication', exact: true })).toBeVisible();
 });
