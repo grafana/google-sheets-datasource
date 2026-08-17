54330dfff6f5df3b0e4f710b83e73d8ae015a3db
ismail simsek <ismailsimsek09@gmail.com>
2026-08-17 22:50:04 +0300

    eslint update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,41 +1,39 @@
 import { defineConfig } from 'eslint/config';
 import baseConfig from './.config/eslint.config.mjs';
-import prettier from 'eslint-plugin-prettier';
 
 export default defineConfig([
   {
     ignores: [
+      '**/logs',
+      '**/*.log',
+      '**/npm-debug.log*',
+      '**/yarn-debug.log*',
+      '**/yarn-error.log*',
+      '**/.pnpm-debug.log*',
       '**/node_modules/',
-      '**/coverage/',
-      '**/ci/',
+      '.yarn/cache',
+      '.yarn/unplugged',
+      '.yarn/build-state.yml',
+      '.yarn/install-state.gz',
+      '**/.pnp.*',
+      '**/pids',
+      '**/*.pid',
+      '**/*.seed',
+      '**/*.pid.lock',
+      '**/lib-cov',
+      '**/coverage',
       '**/dist/',
-      '**/.idea/',
+      '**/artifacts/',
+      '**/work/',
+      '**/ci/',
       'test-results/',
       'playwright-report/',
       'blob-report/',
       'playwright/.cache/',
       'playwright/.auth/',
+      '**/.idea',
       '**/.eslintcache',
-      '**/__debug_bin',
-      '**/mage_output_file.go',
-      'provisioning/',
-      '**/.pnp.*',
-      '.yarn/*',
-      '!.yarn/patches',
-      '!.yarn/plugins',
-      '!.yarn/releases',
-      '!.yarn/sdks',
-      '!.yarn/versions',
     ],
   },
   ...baseConfig,
-  {
-    plugins: {
-      prettier: prettier,
-    },
-
-    rules: {
-      'prettier/prettier': 'error',
-    },
-  },
 ]);
